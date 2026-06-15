52fd28334fcf665f7ec80a0100648e9b7f8a4848
JoePallister <joepallister@gmail.com>
2026-06-15 16:23:30 +0100

    testing setting headers

--- headers/app2/server.py
+++ headers/app2/server.py
@@ -4,12 +4,13 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         print("Headers received:")
+        header_string = ""
         for name, value in self.headers.items():
-            print(f"{name}: {value}")
+            header_string += f"{name}: {value}\n"
 
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"APP2 - OK")
+        self.wfile.write(b"APP2 header string:\n" + header_string.encode())
 
 
 HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
